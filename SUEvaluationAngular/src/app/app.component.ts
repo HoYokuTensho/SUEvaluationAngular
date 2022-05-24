3615384307dd9443a969b596ff0ff69a0f57d2cc
cMoi <cMonEmail@mail.com>
2022-05-24 12:37:19 +0200

    Exercice 4

--- SUEvaluationAngular/src/app/app.component.ts
+++ SUEvaluationAngular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Statistique } from './models/Statistique';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SUEvaluationAngular';
+
+  statistique1 : Statistique = 
+  { 
+    identifiant: "fa1f5f40-be3b-11eb-91ec-7f5875ecfb46",
+    titre: "Démographie en France",
+    valeur : "60M"
+  }
+
+  statistique2 : Statistique  = 
+  { 
+    identifiant: "fa1f5f40-be3b-11eb-91ec-7f5875ecfb47",
+    titre: "Démographie en Allemagne",
+    valeur : "83M"
+  }
 }
